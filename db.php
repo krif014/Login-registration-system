d31b7a3bd9f999b582f0c5df5c121e8f302cd08a
krif014 <krif014@gmail.com>
2026-05-03 10:00:00 +0200

    Updating the db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php 
 $host = "localhost";
-$db = "krif-login";
+$db = "";
 $user = "root";
-$password = "k200808k";
+$password = "";
 $charset = "utf8mb4";
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options =[
